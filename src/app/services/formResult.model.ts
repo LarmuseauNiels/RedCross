a95f169da0c7454c23d4b8f09d7551ad7014749c
Amish <amish.de.meirleir@gmail.com>
2020-06-09 21:16:20 +0200

    formResult model implmented

---
+++ src/app/services/formResult.model.ts
@@ -0,0 +1,31 @@
+export class FormResult{
+
+  // Page 1 - User
+  gender: string;
+  ageRage: string;
+  education: string;
+  hadFATraining: boolean;
+  numberOfFATraining: number | null;
+  trainingByRC: boolean | null;
+  otherTrainingProvider: string | null;
+
+  // Page 2 - Map
+  longitude: string;
+  latitude: string;
+
+  // Page 3 - Setting
+  setting: string;
+
+  // Page 4 - Injury
+  injury: string[];
+
+  // Page 5 - Assistance
+  assistance: string[];
+  confidentApplyingFA: number;
+
+  // Page 6 - Professional help
+  phNeeded: boolean;
+  phType: string[];
+  phTimeToArriveMs: number;
+  hospitalisationRequired: boolean;
+}
