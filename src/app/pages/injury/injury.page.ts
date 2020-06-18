8b1b43d99da88f5f62e2fa546b447a1a119dc9c9
BerbeDB <berbedeblauwe@gmail.com>
2020-06-18 17:21:58 +0200

    injury list

--- src/app/pages/injury/injury.page.ts
+++ src/app/pages/injury/injury.page.ts
@@ -24,13 +24,26 @@ export class InjuryPage implements OnInit {
         return;
       }
       this.injuryTiles = [];
-      this.injuryTiles.push(new TileModel('Fracture', 'assets/icon/injury/fracture.png'));
-      this.injuryTiles.push(new TileModel('Cuts', 'assets/icon/injury/cuts.png'));
-      this.injuryTiles.push(new TileModel('Snake bite', 'assets/icon/injury/snake_bite.png'));
-      this.injuryTiles.push(new TileModel('Burn', 'assets/icon/injury/burn.png'));
-      this.injuryTiles.push(new TileModel('Poisoning', 'assets/icon/injury/poisoning.png'));
-      this.injuryTiles.push(new TileModel('Head injury', 'assets/icon/injury/headinjury.png'));
+      this.injuryTiles.push(new TileModel('General', 'assets/icon/injury/general.png'));
+      this.injuryTiles.push(new TileModel('Stroke', 'assets/icon/injury/stroke.png'));
       this.injuryTiles.push(new TileModel('Unconsciousness', 'assets/icon/injury/unconsciosness.png'));
+      this.injuryTiles.push(new TileModel('Choking', 'assets/icon/injury/choking.png'));
+      this.injuryTiles.push(new TileModel('Chest Discomfort', 'assets/icon/injury/chest_discomfort.png'));
+      this.injuryTiles.push(new TileModel('Severe bleeding', 'assets/icon/injury/severe_bleeding.png'));
+      this.injuryTiles.push(new TileModel('Poisoning', 'assets/icon/injury/poisoning.png'));
+      this.injuryTiles.push(new TileModel('Skin Wounds', 'assets/icon/injury/skin_wounds.png'));
+      this.injuryTiles.push(new TileModel('Skin Wounds with Embedded Object', 'assets/icon/injury/skin_wounds_with_embedded_object.png'));
+      this.injuryTiles.push(new TileModel('Burns', 'assets/icon/injury/burns.png'));
+      this.injuryTiles.push(new TileModel('Injuries to muscles, joints or limbs', 'assets/icon/injury/injuries_to_muscles_joints_or_limbs.png'));
+      this.injuryTiles.push(new TileModel('Injuries to head, neck or back', 'assets/icon/injury/injury_head_neck_or_back.png'));
+      this.injuryTiles.push(new TileModel('Eye Injuries', 'assets/icon/injury/eye_injury.png'));
+      this.injuryTiles.push(new TileModel('Nose Bleed', 'assets/icon/injury/nose_bleed.png'));
+      this.injuryTiles.push(new TileModel('Coronavirus', 'assets/icon/injury/coronavirus.png'));
+      this.injuryTiles.push(new TileModel('Fainting', 'assets/icon/injury/fainting.png'));
+      this.injuryTiles.push(new TileModel('Fever', 'assets/icon/injury/fever.png'));
+      this.injuryTiles.push(new TileModel('Fits', 'assets/icon/injury/fits.png'));
+      this.injuryTiles.push(new TileModel('Diarrhoea', 'assets/icon/injury/diarrhoea.png'));
+      this.injuryTiles.push(new TileModel('Measles', 'assets/icon/injury/measles.png'));
       this.injuryTiles.push(new TileModel('Other', 'assets/icon/other.png'));
   }
 
